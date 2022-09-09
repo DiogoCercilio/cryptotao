4744d554439a6d5660fda7780e7b06e8125f0cdb
Diogo Silva <diogocercilio@gmail.com>
2022-09-09 16:17:56 -0300

    [part1-step-2] - Add a simple Hello world test - Scrapping CryptoTao website. Added Vscode config for debugging

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -19,4 +19,16 @@ describe('AppController', () => {
       expect(appController.getHello()).toBe('Hello World!');
     });
   });
+
+  describe('root', () => {
+    it('should visit CryptoTao App and test some stuff"', async () => {
+      const data = await appController.checkCryptoTaoApp()
+
+      expect(data.title).toEqual('CryptoTao')
+      expect(data.subtitle).toEqual('A better way tounlock your digital assets')
+      expect(data.callToActionBtn).toEqual('Join the waitlist')
+      expect(data.banner).toEqual('https://tailwindui.com/img/component-images/cloud-illustration-teal-cyan.svg')
+    });
+  });
+
 });
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { HttpCode, Injectable } from '@nestjs/common';
+const got = require('got');
+const cheerio = require('cheerio');
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+
+  @HttpCode(200)
+  async checkCryptoTaoApp() {
+    return got('https://www.cryptotao.com').then(response => {
+      const $ = cheerio.load(response.body);
+
+      return {
+        title: $('h1').eq(0).text(),
+        subtitle: $('h1').eq(1).text(),
+        callToActionBtn: $('button').text(),
+        banner: $('img').attr('src')
+      }
+    }).catch(err => {
+      return {
+        msg: 'Error scrapping the page'
+      }
+    });
   }
 }
